e51f713ce9a4e7b37b9deca9d7dc415a19604682
B4lex <b4lex@github.com>
2021-12-05 20:25:37 +0200

    Add Room model. Change admin

--- backend/chat/admin.py
+++ backend/chat/admin.py
@@ -3,11 +3,11 @@ from django.contrib import admin
 from . import models
 
 
-@admin.register(models.ChatUser)
-class ChatUserAdmin(admin.ModelAdmin):
-    pass
-
-
 @admin.register(models.Message)
 class MessageAdmin(admin.ModelAdmin):
     list_display = ('sent', 'sender')
+
+
+@admin.register(models.Room)
+class RoomAdmin(admin.ModelAdmin):
+    list_display = ('name',)
--- backend/chat/models.py
+++ backend/chat/models.py
@@ -4,6 +4,10 @@ from django.db import models
 from authorization.models import ChatUser
 
 
+class Room(models.Model):
+    name = models.CharField(max_length=256)
+
+
 class Message(models.Model):
     content = models.TextField()
     sender = models.ForeignKey(
@@ -12,6 +16,11 @@ class Message(models.Model):
         on_delete=models.CASCADE
     )
     sent = models.DateTimeField(auto_now_add=True)
+    room = models.ForeignKey(
+        Room,
+        related_name='messages',
+        on_delete=models.CASCADE
+    )
 
     def __str__(self):
         return f'{self.sender} ({self.sent})'
