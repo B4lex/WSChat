b5d0cd3b55572cbf8a9e5d0de126594b9e01f572
B4lex <b4lexxx@gmail.com>
2022-01-29 16:33:35 +0200

    Add profile page upload avatar button. Add MDI support

--- frontend/src/pages/Profile.vue
+++ frontend/src/pages/Profile.vue
@@ -3,7 +3,15 @@
     <Header />
     <v-container class="profile-wrapper">
       Your name is {{ userInfo.username }}!
-      <img :src="userInfo.avatar" />
+      <div class="avatar">
+        <a class="upload-photo">
+          <div>
+            <v-icon color="white" class="d-block">mdi-upload</v-icon>
+            Upload your photo
+          </div>
+        </a>
+        <img :src="userInfo.avatar">
+      </div>
     </v-container>
   </div>
 </template>
@@ -26,16 +34,56 @@ export default {
 
 <style scoped>
 .profile-wrapper {
+  box-sizing: content-box;
   background-color: rgba(1, 1, 1, 0.2);
   border-radius: 10px;
   color: white;
+  z-index: 1;
 }
-.profile-wrapper img {
-  display: block;
+.avatar {
+  margin: 2rem auto;
   width: 400px;
   height: 400px;
   border-radius: 50%;
   border: black 2px solid;
-  margin: 2rem auto;
+}
+
+.avatar > img {
+  display: block;
+  width: 100%;
+  height: 100%;
+  border-radius: 50%;
+}
+
+.avatar:hover > img {
+  transition: filter 1s;
+  filter: blur(10px);
+}
+
+.avatar:hover > .upload-photo {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  animation: upload 1s;
+  animation-fill-mode: forwards;
+  animation: appear 1s;
+}
+
+.avatar > .upload-photo {
+  color: white;
+  z-index: 1000;
+  display: none;
+  position: absolute;
+  text-align: center;
+  width: 400px;
+  height: 400px;
+  border-radius: 50%;
+  background: rgba(0, 0, 0, 0.7);
+}
+
+@keyframes appear {
+  0% {
+    background: rgba(0, 0, 0, 0);
+  }
 }
 </style>
--- frontend/src/plugins/vuetify.js
+++ frontend/src/plugins/vuetify.js
@@ -1,5 +1,8 @@
 import Vue from 'vue'
 import Vuetify from 'vuetify'
+
+import '@mdi/font/css/materialdesignicons.css'
+
 import 'vuetify/dist/vuetify.min.css'
 
 Vue.use(Vuetify)
