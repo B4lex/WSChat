3f23f8564935bff4400a679e5fb4ebb027a24cab
Oleksandr <oblakytnyi@gmail.com>
2023-01-28 00:21:28 +0100

    Remove eslint vue-cli plugin

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,2 +1,2 @@
-export const API_ROOT = process.env.API_ROOT || null
-export const WEBSOCKET_ROOT = process.env.WEBSOCKET_ROOT || null
+export const API_ROOT = process.env.VUE_APP_API_ROOT || null
+export const WEBSOCKET_ROOT = process.env.VUE_APP_WEBSOCKET_ROOT || null
