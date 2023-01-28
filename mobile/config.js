5b8da103ddd539764e52ea1d74e024269f72d297
Oleksandr <oblakytnyi@gmail.com>
2023-01-28 12:30:39 +0100

    Enable .env file variables support

--- mobile/config.js
+++ mobile/config.js
@@ -1,2 +1,2 @@
-export const API_ROOT = "https://ecd7-83-142-221-7.eu.ngrok.io/api/";
-export const WEBSOCKET_ROOT = process.env.VUE_APP_WEBSOCKET_ROOT || null;
+export const API_ROOT = process.env.API_ROOT || null;
+export const WEBSOCKET_ROOT = process.env.WEBSOCKET_ROOT || null;
